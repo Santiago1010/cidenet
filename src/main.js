56c9b3ea8a90b925ecd63a4efb2c5b1182804f20
Santiago Correa Aguirre <santiago.c.a_10@hotmail.es>
2022-09-19 16:41:20 -0500

    Configuraciones iniciales

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
-import './style.css'
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/es'
+
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+
+import 'quasar/src/css/index.sass'
+
+import './assets/styles/style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+.use(Quasar, {
+  	plugins: {}, // import Quasar plugins and add here
+  	lang: quasarLang
+})
+.mount('#app')
