02c91ae3735291a174676ccbfccd6ac9a3365de1
Santiago Correa Aguirre <santiago.c.a_10@hotmail.es>
2022-09-20 23:50:37 -0500

    Falta darle funcionalidad al filtro por fecha

--- src/pages/Inicio.vue
+++ src/pages/Inicio.vue
@@ -15,6 +15,36 @@
 			<q-btn label="crear nuevo producto" color="primary" @click="openCreate"><q-tooltip>Haz click para crear un nuevo producto.</q-tooltip></q-btn>
 		</template>
 
+		<template v-slot:top-row>
+			<q-tr>
+				<q-td colspan="100%">
+					<div class="row q-my-xs" style="width: 100%;">
+						<div class="col-12 col-sm-6 q-px-xs">
+							<q-select outlined dense v-model="measuresFilter" label="Filtrar por tipo de medida" :options="measures" @update:model-value="filterPerMeasure">
+								<q-tooltip>Filtrar por tipo de medida.</q-tooltip>
+							</q-select>
+						</div>
+
+						<div class="col-12 col-sm-6 q-px-xs">
+							<q-input outlined dense v-model="datesFilter" label="Filtrar por fecha de creación" mask="date" :rules="['date']" @update:model-value="filterPerDate">
+								<template v-slot:append>
+									<q-icon name="event" class="cursor-pointer">
+										<q-popup-proxy cover transition-show="scale" transition-hide="scale">
+											<q-date v-model="datesFilter">
+												<div class="row items-center justify-end">
+													<q-btn v-close-popup label="Cerrar" color="primary" flat @click="filterPerDate" />
+												</div>
+											</q-date>
+										</q-popup-proxy>
+									</q-icon>
+								</template>
+							</q-input>
+						</div>
+					</div>
+				</q-td>
+			</q-tr>
+		</template>
+
 		<template v-slot:body="props">
 			<q-tr>
 				<q-td key="id" :props="props">
@@ -109,56 +139,62 @@
 
 <script setup>
 	import { ref } from 'vue'
+	import { useQuasar } from 'quasar'
 
 	import { useProductsStore } from '../assets/plugins/ProductsStore.js'
+
+	const $q = useQuasar()
 
 	const productsStore = useProductsStore();
 
 	const filter = ref('')
 
-	var products = productsStore.products
-
 	const columns = [
-		{
-			name: 'id',
-			label: '#',
-			align: 'center',
-			field: 'id',
-			sortable: true	
-		},
-		{
-			name: 'product_name',
-			label: 'Nombre del producto',
-			align: 'left',
-			field: 'product_name',
-			sortable: true	
-		},
-		{
-			name: 'measure',
-			label: 'Tipo de medida',
-			align: 'center',
-			field: 'measure',
-			sortable: true	
-		},
-		{
-			name: 'creation_date',
-			label: 'Fecha de creación',
-			align: 'center',
-			field: 'creation_date',
-			sortable: true	
-		},
-		{
-			name: 'actions',
-			label: 'Acciones',
-			align: 'center'
-		}
+	{
+		name: 'id',
+		label: '#',
+		align: 'center',
+		field: 'id',
+		sortable: true	
+	},
+	{
+		name: 'product_name',
+		label: 'Nombre del producto',
+		align: 'left',
+		field: 'product_name',
+		sortable: true	
+	},
+	{
+		name: 'measure',
+		label: 'Tipo de medida',
+		align: 'center',
+		field: 'measure',
+		sortable: true	
+	},
+	{
+		name: 'creation_date',
+		label: 'Fecha de creación',
+		align: 'center',
+		field: 'creation_date',
+		sortable: true	
+	},
+	{
+		name: 'actions',
+		label: 'Acciones',
+		align: 'center'
+	}
 	]
+
+	var backupComplete = productsStore.products
 
 	const measures = ['Una vez', 'Semanalmente', 'Diariamente', 'Raramente', 'Con frecuencia']
 
 	const form = ref(false)
 	const confirm = ref(false)
 	const action = ref('editar')
+
+	const measuresFilter = ref(null)
+	const datesFilter = ref(null)
 
 	const productsData = ref({
 		id: productsStore.products.length + 1,
@@ -168,19 +204,15 @@
 	})
 
 	const openEdit = (id) => {
-		console.clear()
-
 		form.value = true
 		action.value = 'editar'
 
-		let product = products.filter((product) => product.id === id)
+		let product = productsStore.products.filter((product) => product.id === id)
 
 		productsData.value.id = id
 		productsData.value.product_name = product[0].product_name
 		productsData.value.measure = product[0].measure
 		productsData.value.creation_date = product[0].creation_date
-
-		//console.clear()
 	}
 
 	const openCreate = () => {
@@ -219,23 +251,111 @@
 	}
 
 	const finishAction = () => {
-		let index = products.findIndex(product => product.id === productsData.value.id)
+		let index = productsStore.products.findIndex(product => product.id === productsData.value.id)
+		let length = productsStore.products.length
 
 		switch (action.value) {
 			case 'crear':
-				productsStore.createProduct(productsData.value)
-				closeEveryDialog()
-				break;
+			createProduct(length)
+			break;
 
 			case 'editar':
-				productsStore.updateProduct(index, productsData.value)
-				closeEveryDialog()
-				break;
+			updateProduct(index, productsData.value)
+			break;
 
 			case 'eliminar':
-				productsStore.deleteProduct(index)
-				closeEveryDialog()
-				break;
+			deleteProduct(index, length)
+			break;
 		}
+
+		console.clear()
+	}
+
+	const createProduct = (length) => {
+		productsStore.createProduct(productsData.value)
+
+		if (productsStore.products.length > length) {
+			$q.notify({
+				color: 'green-4',
+				textColor: 'white',
+				icon: 'check',
+				message: 'Se ha creado el producto con éxito.'
+			})
+
+			backupComplete = productsStore.products
+			closeEveryDialog()
+		}else {
+			$q.notify({
+				color: 'red-5',
+				textColor: 'white',
+				icon: 'cancel',
+				message: 'No se ha podido crear el producto nuevo.'
+			})
+		}
+	}
+
+	const updateProduct = (index, product) => {
+		let backup = [productsData.value.product_name, productsData.value.measure]
+
+		productsStore.updateProduct(index, product)
+
+		if (productsStore.products.product_name !== backup[0] && productsStore.products.product_name !== [1]) {
+			$q.notify({
+				color: 'green-4',
+				textColor: 'white',
+				icon: 'check',
+				message: 'Se ha actualizado el producto con éxito.'
+			})
+
+			closeEveryDialog()
+		}else {
+			$q.notify({
+				color: 'red-5',
+				textColor: 'white',
+				icon: 'check',
+				message: 'No se ha podido actualizar el producto.'
+			})
+		}
+
+		backupComplete = productsStore.products
+	}
+
+	const deleteProduct = (index, length) => {
+		productsStore.deleteProduct(index)
+		
+		if (productsStore.products.length <= length) {
+			$q.notify({
+				color: 'green-4',
+				textColor: 'white',
+				icon: 'check',
+				message: 'Se ha eliminado el producto con éxito.'
+			})
+
+			closeEveryDialog()
+		}else {
+			$q.notify({
+				color: 'red-5',
+				textColor: 'white',
+				icon: 'cancel',
+				message: 'No se ha eliminado el producto.'
+			})
+		}
+
+		backupComplete = productsStore.products
+	}
+
+	const filterPerMeasure = () => {
+		productsStore.products = backupComplete
+
+		productsStore.products = productsStore.products.filter(product => product.measure === measuresFilter.value)
+	}
+
+	const filterPerDate = () => {
+		productsStore.products = backupComplete
+
+		let newDate = datesFilter.value.split('/')
+		let searchDate = newDate[2] + "/" + newDate[1] + "/" + newDate[0]
+
+		productsData.products = productsStore.products.filter(product => product.creation_date === searchDate)
 	}
 </script>
